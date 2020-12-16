5cbf4c52c7ecda5516f0d9a4824bd77709b1ea8d
BAS <asberyan@gmail.com>
2020-12-16 18:47:02 +0400

    SignalR Project

---
+++ SignalRExample_2/Hubs/NotificationHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalR_Common;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalR_Server.Hubs
+{
+    public class NotificationHub:Hub
+    {
+        public Task SendMessage(Message message)
+        {
+            Debug.WriteLine("******************* Client Connection Id **********************");
+            Debug.WriteLine(Context.ConnectionId);
+            Debug.WriteLine("***************************************************************");
+            return Clients.Others.SendAsync("Send", message);
+        }
+    }
+}
---
+++ SignalR_Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalR_Common;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR_Client
+{
+    class Program
+    {
+        static HubConnection hubConnection;
+
+        static async Task Main(string[] args)
+        {
+            await InitSignalRConnection();
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                Console.WriteLine("Enter your message or command");
+                var userInput = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(userInput)) 
+                    continue;
+                if(userInput == "exit")
+                {
+                    isExit = true;
+                }
+                else
+                {
+                    var message = new Message
+                    {
+                        Title = "Simple message",
+                        Body = userInput
+                    };
+
+                    await hubConnection.SendAsync("SendMessage", message);
+                    Console.WriteLine("Message send");
+                }
+            }
+        }
+
+        private static Task InitSignalRConnection()
+        {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:60960/notification")
+                .Build();
+
+            hubConnection.On<Message>("Send", message => { 
+                Console.WriteLine("New message from server");
+                Console.WriteLine($"Title: {message.Title}");
+                Console.WriteLine($"Body: {message.Body}");
+            });
+
+            return hubConnection.StartAsync();
+        }
+    }
+}
